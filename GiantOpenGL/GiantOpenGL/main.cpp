6ad70792d62f4ab16be5ac608aad25a8324d9caf
giantfay <giantfay@163.com>
2018-11-25 20:05:09 +0800

    Create window

--- GiantOpenGL/GiantOpenGL/main.cpp
+++ GiantOpenGL/GiantOpenGL/main.cpp
@@ -4,8 +4,66 @@
 #include "stb_image.h"
 #include "Shader.h"
 
+#include <iostream>
+
+const int screenWidth = 800;
+const int screenHeight = 600;
+
+void processInput(GLFWwindow* window);
+
 int main()
 {
+	//init glfw
 	glfwInit();
+
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
+	//create windwo
+	GLFWwindow* window = glfwCreateWindow(screenWidth, screenHeight, "GinatOpenGL", nullptr, nullptr);
+	if (!window)
+	{
+		std::cout << "Failed to create glfw window" << std::endl;
+		glfwTerminate();
+		return -1;
+	}
+
+	glfwMakeContextCurrent(window);
+
+	//init glad
+	if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress))
+	{
+		std::cout << "Failed to init glad" << std::endl;
+		return -1;
+	}
+	
+	glfwSetFramebufferSizeCallback(window,
+		[](GLFWwindow* win, int width, int height)
+		{
+			glViewport(0, 0, width, height);
+		});
+
+	//render loop
+	while (!glfwWindowShouldClose(window))
+	{
+		processInput(window);
+
+		glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
+		glClear(GL_COLOR_BUFFER_BIT);
+
+		glfwPollEvents();
+		glfwSwapBuffers(window);
+	}
+
+	glfwTerminate();
 	return 0;
+}
+
+void processInput(GLFWwindow* window)
+{
+	if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
+	{
+		glfwSetWindowShouldClose(window, true);
+	}
 }
