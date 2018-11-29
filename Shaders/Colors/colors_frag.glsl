d0b25925f863f87abc1c5ea8d46336bea4083c4f
giantfay <giantfay@163.com>
2018-11-29 23:13:37 +0800

    Revert "Revert " Phong shading in view space""
    This reverts commit 8afe02fba712807a1cb403651b2b2c3cb047db44.

--- Shaders/Colors/colors_frag.glsl
+++ Shaders/Colors/colors_frag.glsl
@@ -2,6 +2,7 @@
 
 in vec3 FragPos;
 in vec3 Normal;
+in vec3 LightPos;
 
 out vec4 FragColor;
 
@@ -11,20 +12,18 @@ uniform float specularStrength;
 
 uniform vec3 objColor;
 uniform vec3 lightColor;
-uniform vec3 lightPos;
-uniform vec3 viewPos;
 
 void main()
 {
     vec3 ambient = ambientStrength*lightColor;
 
     vec3 normal = normalize(Normal);
-    vec3 lightDir = normalize(lightPos - FragPos);
+    vec3 lightDir = normalize(LightPos - FragPos);
 
     float diff = max(dot(normal, lightDir), 0.0f);
     vec3 diffuse = diffuseStrength * diff * lightColor;
 
-    vec3 viewDir = normalize(viewPos - FragPos);
+    vec3 viewDir = normalize( - FragPos);
     vec3 reflectDir = reflect(-lightDir, normal);
     float spec = pow(max(dot(reflectDir, viewDir), 0.0f), 32);
     vec3 specular = specularStrength * spec * lightColor;
